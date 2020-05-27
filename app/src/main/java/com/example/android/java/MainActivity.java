4db80c4a9ad631e02e46b36ad621d68e4cd45fe1
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-05-27 16:36:12 +1000

    Animate properties without listener

--- app/src/main/java/com/example/android/java/MainActivity.java
+++ app/src/main/java/com/example/android/java/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.java;
 
 import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
@@ -15,42 +16,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private ScrollView mscroll;
-    private TextView mLog;
-
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Initialize the logging comments
-        mscroll = (ScrollView)findViewById(R.id.scrolllog);
-        mLog = (TextView)findViewById(R.id.tvlog);
-        mLog.setText("");
+        imageView = (ImageView)findViewById(R.id.imageView);
     }
 
 
-    public void onRuncodeButtonClick(View view) {
-        final ValueAnimator animator = ValueAnimator.ofFloat(1f, 20f).setDuration(2000);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                displayMessage("timestamp: " + animation.getCurrentPlayTime() +
-                        ", values:"+ animation.getAnimatedValue());
-            }
-        });
-        animator.setRepeatCount(2);
-        animator.setRepeatMode(ValueAnimator.REVERSE);
-        animator.start();
-    }
+    public void onRunButtonClick(View view) {
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(imageView,
+                "scaleX", 1f, 2f)
+                .setDuration(1000);
+        animatorX.start();
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageView,
+                "scaleY", 1f, 2f)
+                .setDuration(1000);
+        animatorY.start();
 
-    public void onClearButtonClick(View view) {
-        mLog.setText("");
-        mscroll.scrollTo(0, mscroll.getBottom());
-    }
-
-    public void displayMessage(String message){
-        mLog.append(message + "\n");
-        mscroll.scrollTo(0, mscroll.getBottom());
     }
 }
