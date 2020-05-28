1ec681a9d1a584dc11f49778549afd64ddea122b
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-05-28 15:59:10 +1000

    Modify animations with interpolators

--- app/src/main/java/com/example/android/java/MainActivity.java
+++ app/src/main/java/com/example/android/java/MainActivity.java
@@ -11,34 +11,38 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
+    private RelativeLayout canvas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = (ImageView)findViewById(R.id.imageView);
+        canvas = (RelativeLayout)findViewById(R.id.animationcanvas);
     }
 
 
-    public void onRunButtonClick(View view) {
-        ObjectAnimator animatorX = ObjectAnimator.ofFloat(imageView,
-                "scaleX", 1f, 2f)
-                .setDuration(1000);
-        ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageView,
-                "scaleY", 1f, 2f)
-                .setDuration(1000);
-        AnimatorSet set = new AnimatorSet();
-       // set.playTogether(animatorX, animatorY);
-        set.playSequentially(animatorX, animatorY);
-        set.setDuration(3000); //duration can be overwritten
-        set.start();
+    public void onButtonClick(View view) {
 
+        int screenHeight = canvas.getHeight();
+        int targetY = screenHeight - imageView.getHeight();
 
+        ObjectAnimator animator = ObjectAnimator.ofFloat(
+                imageView, "y",0, targetY)
+                .setDuration(1000);
+       // animator.setInterpolator(new LinearInterpolator());
+        // animator.setInterpolator(new AnticipateInterpolator());
+        animator.setInterpolator(new BounceInterpolator());
+        animator.start();
     }
 }
